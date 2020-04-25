2f170fae88c3e4eb25a736afe86ade53024a4766
Sue Duclos <sue.duclos@gmail.com>
2020-04-24 21:47:33 -0700

    updated JSDocs function comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// TODO: What is this line doing?
+// DONE: What is this line doing?
 // This line is connecting to the dotenv file
 require('dotenv').config();
 
-// TODO: What is this line doing?
+// DONE: What is this line doing?
 // This line is importing our server and starting the server using a port stored in the environment variables
 require('./lib/server.js').start(process.env.PORT);
